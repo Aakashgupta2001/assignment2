87e25f95f9cc01ef9235025e671a57159a158e63
Aakash Gupta <aakashgupta2001.2909@gmail.com>
2023-02-09 19:17:55 +0530

    FE init and now api return error for invalid input

--- src/app.service.ts
+++ src/app.service.ts
@@ -11,6 +11,13 @@ export class AppService {
 
   PossibleMoves(current_position: string): string[] {
     console.log(current_position);
+    if (!/^[a-h][1-8]$/.test(current_position)) {
+      // we need to throw the error here but i am returning it since i am not making an error handler for this task
+      // throw new Error(
+      //   "Invalid input format, please use format like 'a1' or 'h8'",
+      // );
+      return ['Invalid Input'];
+    }
     const x = current_position.charCodeAt(0) - 97;
     const y = parseInt(current_position[1]) - 1;
     const moves = [];
